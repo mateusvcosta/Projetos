e5a57925f53962834200212108aba58b930a525a
Mateus <mateusvcosta03@gmail.com>
2026-09-10 17:06:19 +0100

    a

--- C#/DesktopApp/obj/Debug/net10.0-windows/DesktopApp.AssemblyInfo.cs
+++ C#/DesktopApp/obj/Debug/net10.0-windows/DesktopApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesktopApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec1784be61584ef8fcca53dc02cae5f46ff1f7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesktopApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesktopApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
